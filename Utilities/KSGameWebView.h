c0da7d62be6f09bca88d1f640f8716ef10b5b5d1
Ben Wheatley <ben@kitsunesoftware.com>
2017-09-17 17:40:46 +0200

    • Better name for KSGameWebView (was KSGameWebViewCreator) • Better type safety for KSGameWebView • Improve construction of KSGameWebView

---
+++ Utilities/KSGameWebView.h
@@ -0,0 +1,32 @@
+//
+//  KSGameWebViewCreator.h
+//  Runestone
+//
+//  Created by Ben Wheatley on 01/05/2011.
+//  Copyright 2011 Kitsune Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+#pragma mark KSGameWebViewCreator
+
+@interface KSGameWebView : NSObject {
+    
+}
+
++(KSGameWebView*) initWithLocalFile:(NSString*)fileNameBase fileNameExtension:(NSString*)fileNameExtension boundingRect:(CGRect)boundingRect;
+
+@end
+
+
+#pragma mark KSGameWebViewDelegate
+
+@interface KSGameWebViewDelegate : NSObject <UIWebViewDelegate> {
+@private
+    
+}
+
++ (KSGameWebViewDelegate *) commonWebViewDelegate;
+
+@end
---
+++ Utilities/KSGameWebView.m
@@ -0,0 +1,82 @@
+//
+//  KSGameWebViewCreator.m
+//  Runestone
+//
+//  Created by Ben Wheatley on 01/05/2011.
+//  Copyright 2011 Kitsune Software. All rights reserved.
+//
+
+#import "KSGameWebView.h"
+
+
+@implementation KSGameWebView
+
++(KSGameWebView*) makeWebViewWithLocalFile:(NSString*)fileNameBase fileNameExtension:(NSString*)fileNameExtension boundingRect:(CGRect)boundingRect
+{
+	return [[KSGameWebView alloc] initWithLocalFile:fileNameBase fileNameExtension:fileNameExtension boundingRect:boundingRect];
+}
+
+-(instancetype) initWithLocalFile:(NSString*)fileNameBase fileNameExtension:(NSString*)fileNameExtension boundingRect:(CGRect)boundingRect {
+    UIWebView *result;
+    
+    result = [[UIWebView alloc] initWithFrame:boundingRect];
+    result.backgroundColor = [UIColor clearColor];
+    [result setOpaque:NO];
+    
+    NSBundle *mainBundle = [NSBundle mainBundle];
+    
+    NSString *path = mainBundle.bundlePath;
+    NSURL *baseURL = [NSURL fileURLWithPath:path];
+    NSString *filePath = [mainBundle pathForResource:fileNameBase ofType:fileNameExtension];
+    NSData *htmlData = [NSData dataWithContentsOfFile:filePath];
+    
+    [result loadData:htmlData MIMEType:@"text/html" textEncodingName:@"UTF-8" baseURL:baseURL];
+    
+    // Remove the gradients that normally appear when user scrolls the UIWebView past the start or end of content region
+    for (UIView* subView in result.subviews) {
+        if ([subView isKindOfClass:[UIScrollView class]]) {
+            for (UIView* shadowView in subView.subviews) {
+                if ([shadowView isKindOfClass:[UIImageView class]]) {
+                    [shadowView setHidden:YES];
+                }
+            }
+        }
+    }
+    
+    // Add a delegate
+	result.delegate = KSGameWebViewDelegate.commonWebViewDelegate;
+	
+    return result;
+}
+
+@end
+
+@implementation KSGameWebViewDelegate
+
++ (KSGameWebViewDelegate *) commonWebViewDelegate {
+	// Pattern from https://stackoverflow.com/questions/2199106/thread-safe-instantiation-of-a-singleton
+	static KSGameWebViewDelegate * commonWebViewDelegate = nil;
+	static dispatch_once_t pred;
+	
+	if (commonWebViewDelegate) {
+		return commonWebViewDelegate;
+	}
+	
+	dispatch_once(&pred, ^{
+		commonWebViewDelegate = [KSGameWebViewDelegate alloc];
+		commonWebViewDelegate = [commonWebViewDelegate init];
+	});
+	
+	return commonWebViewDelegate;
+}
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    if (navigationType == UIWebViewNavigationTypeLinkClicked) {
+        [[UIApplication sharedApplication] openURL:request.URL];
+        return false;
+    }
+    return true;
+}
+
+@end
